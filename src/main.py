e8d1257a61c1d1fba88b981c42973e19fcec69a6
Revan12222 <space3640@gmail.com>
2025-08-03 22:22:35 -0700

    added main and textnode

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    temp_node = TextNode("Hello World","Bold","google.come")
+    print(temp_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, content, text_type, url):
+        self.text = content
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and \
+            self.url == other.url:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
